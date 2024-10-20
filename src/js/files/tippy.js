0450649e0b7d09c152fb90ffef79f2b58bcd13c1
Dimitri <philjke@mail.ru>
2024-10-20 14:16:11 +0700

    доделал каталог, исправил баг с размытием шапки

--- src/js/files/tippy.js
+++ src/js/files/tippy.js
@@ -12,33 +12,45 @@ import 'tippy.js/animations/shift-away.css';
 import '../../scss/libs/tippy.scss';
 
 // Запускаем и добавляем в объект модулей
-const checkedButton = document.querySelector('.alphabet-sort__checked');
+const checkedButton = document.querySelector('.alphabet-sort');
 const sortItems = document.querySelectorAll('.list-sort__item');
 
 // Инициализация tippy
-const popover = tippy('.alphabet-sort__checked', {
-	theme: 'crone',
-	placement: 'bottom',
-	allowHTML: true,
-	interactive: true,
-	arrow: false,
-	animateFill: true,
-	plugins: [animateFill],
-	inertia: true,
-	zIndex: 1,
-	trigger: 'click',
-	content: function () {
-		return document.querySelector('.tip-template');
-	},
-});
-
-// Обработчик клика по элементам сортировки
-sortItems.forEach((item) => {
-	item.addEventListener('click', function () {
-		// Обновляем текст кнопки с выбранным элементом
-		checkedButton.textContent = this.textContent;
-
-		// Закрываем popover (можно вручную скрыть, если нужно)
-		popover[0].hide();
+if (checkedButton && sortItems) {
+	const popover = tippy('.alphabet-sort', {
+		theme: 'crone',
+		placement: 'bottom',
+		allowHTML: true,
+		interactive: true,
+		arrow: false,
+		animateFill: true,
+		plugins: [animateFill],
+		inertia: true,
+		zIndex: 1,
+		trigger: 'click',
+		content: function () {
+			return document.querySelector('.tip-template');
+		},
 	});
-});
+
+	// Обработчик клика по элементам сортировки
+	// Делаем нулевой элемент _active
+	sortItems[0].classList.add('_active');
+
+	sortItems.forEach((item) => {
+		item.addEventListener('click', function () {
+			// Обновляем текст кнопки с выбранным элементом
+			const checkedText = checkedButton.querySelector('.alphabet-sort__checked');
+			if (checkedText) {
+				checkedText.textContent = this.textContent;
+			}
+
+			// Закрываем popover (можно вручную скрыть, если нужно)
+			popover[0].hide();
+
+			// Добавляем/удаляем класс _active
+			sortItems.forEach((el) => el.classList.remove('_active'));
+			this.classList.add('_active');
+		});
+	});
+}
